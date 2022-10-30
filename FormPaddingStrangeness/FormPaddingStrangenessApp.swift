8ce821490f90adaecf4f6522221c270459abd704
Jason Ji <uberjason@gmail.com>
2022-10-29 22:02:43 -0400

    Initial Commit

---
+++ FormPaddingStrangeness/FormPaddingStrangenessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormPaddingStrangenessApp.swift
+//  FormPaddingStrangeness
+//
+//  Created by Jason Ji on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FormPaddingStrangenessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
